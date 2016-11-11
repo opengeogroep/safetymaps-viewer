e9648dfc77b77679a9e3ce979ba0aea78135101f
Geert Plaisier <geert.plaisier@gnamic.nl>
2016-11-11 11:10:11 +0100

    Load Twitter API before loading timeline

--- public/js/dbkjs/modules/incidents/IncidentDetailsWindow.js
+++ public/js/dbkjs/modules/incidents/IncidentDetailsWindow.js
@@ -113,6 +113,20 @@ IncidentDetailsWindow.prototype.addTabClickListener = function() {
 };
 
 IncidentDetailsWindow.prototype.loadTwitterFeed = function(container) {
+    window.twttr = (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0],
+            t = window.twttr || {};
+        if (d.getElementById(id)) return t;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "https://platform.twitter.com/widgets.js";
+        fjs.parentNode.insertBefore(js, fjs);
+        t._e = [];
+        t.ready = function(f) {
+            t._e.push(f);
+        };
+        return t;
+    }(document, "script", "twitter-wjs"));
     twttr.ready(function(twttr) {
         twttr.widgets.createTimeline({
                 sourceType: "collection",
