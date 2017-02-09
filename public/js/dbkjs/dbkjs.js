810ed890f885fc56bcefff254e0f8a6604931221
Matthijs Laan <matthijsln@gmail.com>
2017-02-09 11:50:22 +0100

    set moment locale

--- public/js/dbkjs/dbkjs.js
+++ public/js/dbkjs/dbkjs.js
@@ -18,11 +18,13 @@
  *
  */
 
-/* global OpenLayers, i18n, Proj4js, dbkjsLang */
+/* global OpenLayers, i18n, Proj4js, dbkjsLang, moment */
 
 OpenLayers.ProxyHost = "proxy/?q=";
 OpenLayers.IMAGE_RELOAD_ATTEMPTS = 3;
 Proj4js.defs["EPSG:28992"] = "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.999908 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +no_defs <>";
+
+moment.locale("nl");
 
 var dbkjs = dbkjs || {};
 window.dbkjs = dbkjs;
