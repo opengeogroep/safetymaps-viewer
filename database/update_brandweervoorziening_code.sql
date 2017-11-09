76db34eef7e88aab5824e98b29a01f8130169c27
Matthijs Laan <matthijsln@gmail.com>
2017-11-09 12:14:09 +0100

    fix script

--- database/update_brandweervoorziening_code.sql
+++ database/update_brandweervoorziening_code.sql
@@ -1,5 +1,5 @@
 
-update wfs."Brandweervoorziening" set "Code" = replace("Code", '.', '') where "Code" like '%.%'
+update wfs."Brandweervoorziening" set "Code" = replace("Code", '.', '') where "Code" like '%.%';
 
 update wfs."Brandweervoorziening"
 set "Code" = replace((
@@ -7,12 +7,11 @@ set "Code" = replace((
     from dbk.type_brandweervoorziening t
     where t.gid = "Symbol_Type_ID")
 ,'.','')
-where "Code" is null or "Code" = ''
+where "Code" is null or "Code" = '';
 
 update wfs."Brandweervoorziening" set "Code" = 'Tb1004a' where "Symbol_Type_ID" = 1004;
 update wfs."Brandweervoorziening" set "Code" = 'Tbk5001' where "Symbol_Type_ID" = 1013;
 update wfs."Brandweervoorziening" set "Code" = 'Openwater' where "Symbol_Type_ID" = 1015;
-
 
 
 
