93cc5b88e0b52a090873de14888a8fcc9bbfbad1
Matthijs Laan <matthijsln@gmail.com>
2017-11-29 17:44:53 +0100

    new viewer settings

--- public/js/dbkjs/config/options_mobile.js
+++ public/js/dbkjs/config/options_mobile.js
@@ -1,27 +1,9 @@
-/*!
- *  Copyright (c) 2014 Milo van der Linden (milo@dogodigi.net)
- *
- *  This file is part of safetymapDBK
- *
- *  safetymapDBK is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  safetymapDBK is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with safetymapDBK. If not, see <http://www.gnu.org/licenses/>.
- *
- */
+
 var dbkjs = dbkjs || {};
 window.dbkjs = dbkjs;
 
 OpenLayers.IMAGE_RELOAD_ATTEMPTS = 3;
-OpenLayers.Lang.nl = OpenLayers.Util.applyDefaults({'Scale = 1 : ${scaleDenom}': "Schaal 1 : ${scaleDenom}"});
+OpenLayers.Lang["nl"] = OpenLayers.Util.applyDefaults({'Scale = 1 : ${scaleDenom}': "Schaal 1 : ${scaleDenom}"});
 OpenLayers.Lang.setCode("nl");
 Proj4js.defs["EPSG:28992"] = "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.999908 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +no_defs <>";
 
@@ -37,21 +19,30 @@ dbkjs.options = {
             numDigits: 0
         }
     },
-    VERSION: "2.7",
-    RELEASEDATE: "01-09-2014",
-    APPLICATION: "safetymapsDBK",
+    VERSION: "_VERSION_",
+    RELEASEDATE: "_DATE_",
+    APPLICATION: "Falck voertuigviewer 3.5",
     REMARKS: "",
     INFO: "",
     zoom: 13,
 
+    // If false labels are shown on select and temporary render intents
+    alwaysShowInformationLabels: false,
+
     // Set to true to enable style scaling according to map scale
-    styleScaleAdjust: true,
+    styleScaleAdjust: false,
     // Scale at which scaled values are returned as original
     originalScale: 595.2744,
     // User style value adjustment (before scaling)
     styleSizeAdjust: 0,
 
     alwaysShowDbkFeature: true,
+
+    noLineScaling: true,
+
+    featureLabelResolution: 6.72,
+
+    dbkLayersMinResolution: 0,
 
     /**
      * PDOK settings, particular for the Netherlands national geodata services
@@ -74,14 +65,16 @@ dbkjs.options = {
     },
     map: {
         options : {
-        theme: null,
-        controls: [new OpenLayers.Control.Navigation()],
-        div: 'mapc1map1',
-        projection: new OpenLayers.Projection("EPSG:28992"),
-        resolutions: [3440.64, 1720.32, 860.16, 430.08, 215.04, 107.52, 53.76, 26.88, 13.44, 6.72, 3.36, 1.68, 0.84, 0.42, 0.210, 0.105, 0.0525, 0.02625, 0.013125, 0.0065625],
-        xy_precision: 3,
-        maxExtent: new OpenLayers.Bounds(-65200.96, 242799.04, 375200.96, 68320096),
-        units: "m"
+            theme: null,
+            controls: [new OpenLayers.Control.Navigation()],
+            div: 'mapc1map1',
+            projection: new OpenLayers.Projection("EPSG:28992"),
+            resolutions: [3440.64, 1720.32, 860.16, 430.08, 215.04, 107.52, 53.76, 26.88, 13.44, 6.72, 3.36, 1.68, 0.84, 0.42, 0.210, 0.105, 0.0525, 0.02625, 0.013125, 0.0065625],
+            xy_precision: 3,
+            maxExtent: new OpenLayers.Bounds(-65200.96, 242799.04, 375200.96, 68320096),
+            units: "m",
+            zoomMethod: null,
+            fractionalZoom: true
         }
     }
 };
@@ -98,12 +91,15 @@ if (dbkjs.options.projection.code === 'EPSG:28992') {
 dbkjs.options.baselayers = [
     new OpenLayers.Layer.TMS(
             "Openbasiskaart",
-            "tms",
+            "https://openbasiskaart.nl/mapcache/tms/",
             {
-                layername: '', type: "png", serviceVersion: "",
-                gutter: 0, buffer: 0, isBaseLayer: true, transitionEffect: 'resize',
+                layername: 'osm-nb@rd', type: "png", serviceVersion: '1.0.0',
+                metadata:{pl:"00"},
+                gutter: 0, buffer: 0,
+                isBaseLayer: true, transitionEffect: 'resize',
                 tileOrigin: new OpenLayers.LonLat(-285401.92, 22598.08),
-                resolutions: dbkjs.options.pdok.resolutions,
+                serverResolutions: [3440.64, 1720.32, 860.16, 430.08, 215.04, 107.52, 53.76, 26.88, 13.44,
+6.72, 3.36, 1.68, 0.84, 0.42, 0.21, 0.105, 0.0525, 0.02625, 0.013125, 0.0065625, 0.00328125, 0.001640625],
                 zoomOffset: 0,
                 units: "m",
                 maxExtent: new OpenLayers.Bounds(-285401.92, 22598.08, 595401.92, 903401.92),
@@ -117,6 +113,7 @@ dbkjs.options.baselayers = [
             dbkjs.options.pdok.tms.TMS,
             {
                 layername: 'brtachtergrondkaart',
+                metadata:{pl: "01"},
                 isBaseLayer: true,
                 displayInLayerSwitcher: true,
                 type: 'png',
@@ -130,9 +127,10 @@ dbkjs.options.baselayers = [
     ),
     new OpenLayers.Layer.TMS(
             'Topografische kaart 1:10.000 - top10nl (PDOK)',
-            dbkjs.options.pdok.tms.TMS,
+            'https://geodata.nationaalgeoregister.nl/tiles/service/tms/',
             {
-                layername: 'top10nl',
+                layername: 'top10nlv2/EPSG:28992',
+                metadata:{pl: "02"},
                 isBaseLayer: true,
                 displayInLayerSwitcher: true,
                 type: 'png',
@@ -145,3 +143,23 @@ dbkjs.options.baselayers = [
             }
     )
 ];
+
+dbkjs.options.searchTabs = true;
+
+dbkjs.options.zoomToPandgeometrie = true;
+dbkjs.options.zoomToPandgeometrieMargin = 50;
+
+dbkjs.options.showZoomButtons = true;
+
+dbkjs.options.enableSplitScreen = true;
+dbkjs.options.splitScreenChecked = true;
+dbkjs.options.splitScreenSwitch = true;
+
+dbkjs.options.autoFeatureUpdateInterval = 3600000;
+
+$("#zoom_extent").hide();
+
+dbkjs.options.featureInfoMaxScale = 2381.0976;
+
+dbkjs.options.minTouchMoveEndDistance = 5;
+
