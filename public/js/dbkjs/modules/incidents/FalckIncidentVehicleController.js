d5f6a7d82505410e81972b3aa0ec68ae134ba00d
mvdstruijk <m_struijk1990@hotmail.nl>
2018-04-16 14:23:16 +0200

    Added the vehicleController for the Falck service

---
+++ public/js/dbkjs/modules/incidents/FalckIncidentVehicleController.js
@@ -0,0 +1,125 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function FalckIncidentVehicleController(controller) {
+    var me = this;
+    me.controller = controller;
+    me.options = dbkjs.options.incidents;
+    var selectLayers = [];
+    me.vehiclePositionLayer = new VehiclePositionLayer();
+
+    selectLayers.push(me.vehiclePositionLayer.layer);
+    //me.addSelectLayers(selectLayers);
+    me.getEenheidLocatie();
+
+    window.setInterval(function () {
+        if ((window.localStorage.getItem("VehiclePositionLayer.hidden") === "true")) {
+            //me.getEenheidLocatieIncident();
+        } else {
+            me.getEenheidLocatie();
+        }
+    }, 30000);
+}
+;
+
+FalckIncidentVehicleController.prototype.getEenheidLocatie = function () {
+
+    var me = this;
+    $.ajax(me.options.incidentsUrl + "/eenheidlocatie?extended=false", {
+        dataType: "json"
+    })
+            .fail(function (jqXHR, textStatus, errorThrown) {
+                console.log(jqXHR, textStatus, errorThrown);
+            })
+            .done(function (data, textStatus, jqXHR) {
+                console.log("eenheden", data);
+                me.updateVehicles(data.features);
+            });
+};
+
+FalckIncidentVehicleController.prototype.getEenheidLocatieIncident = function (betrokkenEenheden) {
+    console.log("Betrokken eenheden", betrokkenEenheden);
+    var me = this;
+    var features = [];
+    var calls = [];
+    for (var i = 0; i < betrokkenEenheden.length; i++) {
+        var call = $.ajax(me.options.incidentsUrl + "/eenheidlocatie?id=" + betrokkenEenheden[i], {
+            dataType: "json",
+            voertuigNummer: betrokkenEenheden[i]
+        })
+                .fail(function (jqXHR, textStatus, errorThrown) {
+                    console.log(jqXHR, textStatus, errorThrown);
+                })
+                .done(function (data, textStatus, jqXHR) {
+                    if(data.features.length > 0){
+                        features.push(data.features[0]);
+                    }else {
+                        console.log("Er is geen eenheidlocatie beschikbaar voor voertuig nummer:"+this.voertuigNummer);
+                    }
+                });
+        calls.push(call);
+    }
+    $.when.apply($, calls).then(function () {
+        me.updateVehicles(features);
+    });
+};
+
+FalckIncidentVehicleController.prototype.updateVehicles = function (features) {
+    var me = this;
+    var transformedFeatures = me.transformFeaturesForVehiclePositionLayer(features);
+    console.log("transformedEenheden: ", transformedFeatures);
+    me.vehiclePositionLayer.features(transformedFeatures);
+};
+
+FalckIncidentVehicleController.prototype.transformFeaturesForVehiclePositionLayer = function (features) {
+    var transformedFeatures = [];
+    for (var i = 0; i < features.length; i++) {
+            var feat = features[i];
+            feat.attributes = feat.properties;
+            delete feat.properties;
+            feat.attributes.IncidentID = (feat.attributes.incidentNummer === null) ? "" : feat.attributes.incidentNummer;
+            //feat.attributes.IncidentID=null;
+            delete feat.attributes.incidentNummer;
+            feat.attributes.Voertuigsoort = "";
+            feat.attributes.Roepnummer = feat.attributes.id;
+            feat.attributes.Speed = feat.attributes.speed;
+            //feat.attributes.Speed = 35;
+            feat.attributes.Direction = feat.attributes.heading;
+            feat.geometry = new OpenLayers.Geometry.Point(feat.geometry.coordinates[0], feat.geometry.coordinates[1]);
+            var feature = new OpenLayers.Feature.Vector(feat.geometry, feat.attributes);
+            transformedFeatures[i] = feature;     
+    }
+    return transformedFeatures;
+};
+
+FalckIncidentVehicleController.prototype.addSelectLayers = function (layers) {
+    dbkjs.selectControl.deactivate();
+    dbkjs.selectControl.setLayer((dbkjs.selectControl.layers || dbkjs.selectControl.layer).concat(layers));
+    dbkjs.selectControl.activate();
+};
+
+FalckIncidentVehicleController.prototype.incidentFound = function (incidenten) {
+    console.log("incidenten: ", incidenten);
+    var betrokkenEenheden = [];
+    var me = this;
+    if ((window.localStorage.getItem("VehiclePositionLayer.hidden") === "true")) {
+        if (me.controller.incidentId) {
+            me.vehiclePositionLayer.visibility = true;
+            me.vehiclePositionLayer.layer.setVisibility(true);
+            me.vehiclePositionLayer.layer2.setVisibility(true);
+            for (var i = 0; i < incidenten.BetrokkenEenheden.length; i++) {
+                var eenheid = incidenten.BetrokkenEenheden[i];
+                betrokkenEenheden.push(eenheid.Roepnaam);
+            }
+            me.getEenheidLocatieIncident(betrokkenEenheden);
+        } else {
+            me.vehiclePositionLayer.visibility = false;
+            me.vehiclePositionLayer.layer.setVisibility(false);
+            me.vehiclePositionLayer.layer2.setVisibility(false);
+        }
+    }
+};
