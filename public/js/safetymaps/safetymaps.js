783ab820afef2ea32738804681bcfd53d7020cd4
Matthijs Laan <matthijsln@gmail.com>
2018-12-05 17:17:39 +0100

    add api for tabs in separated windows or common window

--- public/js/safetymaps/safetymaps.js
+++ public/js/safetymaps/safetymaps.js
@@ -31,10 +31,6 @@
  */
 var safetymaps = safetymaps || {};
 
-(function initSafetyMaps() {
-    safetymaps.selectedClusterFeature = null;
-})();
-
 /**
  * Make object providers deselect any selected object.
  */
@@ -72,3 +68,160 @@ safetymaps.selectObject = function(clusterFeature, zoom) {
         console.log("No object selected by object_select listeners");
     }
 };
+
+safetymaps.infoWindow = {
+    windowByTabs: null,
+    commonWindow: null,
+    windows: null,
+    separateWindowMode: null,
+
+    initialize: function(separateWindowMode) {
+        //console.log("infoWindow: initialize, separate windows: " + !!separateWindowMode);
+
+        this.separateWindowMode = separateWindowMode;
+        if(this.separateWindowMode) {
+            this.windowByTabs = {};
+            this.windows = {};
+        } else {
+            this.commonWindow = new SplitScreenWindow("commonInfoWindow");
+            this.commonWindow.createElements(); // no title
+            this.createTabElements(this.commonWindow);
+        }
+    },
+
+    createTabElements: function(window) {
+        var div = $("<div class='tabbable tabs-bottom'></div>");
+        window.tabContent = $("<div class='tab-content'></div>");
+        window.tabs = $("<ul class='nav nav-pills'></ul>");
+        div.append(window.tabContent);
+        div.append(window.tabs);
+        window.getView().append(div);
+    },
+
+    /**
+     * Create and add a new window when not using a common window. Returns the
+     * newly created SplitScreenWindow or the common window.
+     *
+     * @param {string} windowId Name for the window
+     * @param {string} title Title text of the window, shown when window contains a single tab and showSingleTab is false
+     * @param {boolean} showSingleTab Whether to show tabs even if window only has single tab. If false title is also shown.
+     * @returns {SplitScreenWindow}
+     */
+    addWindow: function(windowId, title, showSingleTab) {
+        var window;
+        if(this.separateWindowMode) {
+            //console.log("infoWindow: add window " + windowId);
+            window = new SplitScreenWindow(windowId);
+            window.createElements(title);
+            this.createTabElements(window);
+            window.showSingleTab = showSingleTab;
+            this.windows[windowId] = window;
+        } else {
+            console.log("infoWindow: add window " + windowId + " [ignored, integrated window mode]");
+            window = this.commonWindow;
+        }
+        return window;
+    },
+
+    /**
+     * Add a new tab to a window.
+     * @param {string} windowId id of the window to add the tab to
+     * @param {string} tabId DOM id for tab
+     * @param {string} label the label for the a element
+     * @param {string} clazz CSS class for the tab
+     * @param {string} div tab contents
+     * @param {string} position Position in tab list, either first, last or undefined/null (meaning in order of calling addTab())
+     */
+    addTab: function(windowId, tabId, label, clazz, div, position) {
+        var window = this.separateWindowMode ? this.windows[windowId] : this.commonWindow;
+
+        if(window) {
+            //console.log("infoWindow: window " + windowId + ", add tab " + tabId + " (class " + clazz + ")" + (position ? ", position " + position : ""));
+
+            $("#tab_" + tabId).remove();
+            $("#tab_li_" + tabId).remove();
+
+            var active = window.tabs.find("li").length === 0 ? "active" : "";
+            var tab = $("<div class='tab-pane " + clazz + " " + active + "' id='tab_" + tabId + "'></div>");
+            tab.append(div);
+            window.tabContent.append(tab);
+            var li = "<li class='" + clazz + " " + active + " position_" + position + "' id='tab_li_" + tabId + "'><a data-toggle='tab' href='#tab_" + tabId + "'>" + label + "</a></li>";
+            if(position === "first") {
+                window.tabs.prepend(li);
+            } else if(position !== "last") {
+                var last = window.tabs.find(".position_last");
+                if(last.length > 0) {
+                    $(li).insertBefore(last);
+                } else {
+                    window.tabs.append(li);
+                }
+            } else {
+                window.tabs.append(li);
+            }
+
+
+        } else {
+            console.log("infoWindow: ERROR: cannot add tab '" + tabId + "' tab to unknown window " + windowId);
+        }
+    },
+
+    removeTab: function(windowId, tabId) {
+        var window = this.separateWindowMode ? this.windows[windowId] : this.commonWindow;
+        if(window) {
+            //console.log("infoWindow: remove tab " + tabId);
+
+            window.tabContent.find(".tab-pane #tab_" + tabId).remove();
+            window.tabs.find("li. #tab_li_" + tabId).remove();
+        } else {
+            console.log("infoWindow: ERROR: cannot remove tab '" + tabId + "' tab from unknown window " + windowId);
+        }
+    },
+
+    removeTabs: function(windowId, clazz) {
+        var window = this.separateWindowMode ? this.windows[windowId] : this.commonWindow;
+        if(window) {
+            //console.log("infoWindow: remove tabs for class " + clazz);
+
+            window.tabContent.find(".tab-pane." + clazz).remove();
+            window.tabs.find("li." + clazz).remove();
+        } else {
+            console.log("infoWindow: ERROR: cannot remove '" + clazz + "' tabs from unknown window " + windowId);
+        }
+    },
+
+    showTab: function(windowId, tabId, showWindow) {
+        var window = this.separateWindowMode ? this.windows[windowId] : this.commonWindow;
+        if(window) {
+            //console.log("infoWindow: show tab " + tabId);
+            window.tabContent.find(".tab-pane.active").removeClass("active");
+            window.tabs.find("li.active").removeClass("active");
+            window.tabContent.find("#tab_" + tabId).addClass("active");
+            window.tabs.find("#tab_li_" + tabId).addClass("active");
+
+            if(this.separateWindowMode && !window.showSingleTab) {
+                if(window.tabs.find("li").length === 1) {
+                    window.tabs.hide();
+                }
+            } else {
+                window.tabs.show();
+            }
+
+            if(showWindow) {
+                window.show();
+            }
+        } else {
+            console.log("infoWindow: ERROR: cannot show tab '" + tabId + "' tab for unknown window " + windowId);
+        }
+    }
+};
+
+(function initSafetyMaps() {
+    safetymaps.selectedClusterFeature = null;
+
+    // TODO: replace /api/organisation.json for global settings
+
+    // TODO: other event, after settings and i18n init
+    $(dbkjs).one("dbkjs_init_complete", function() {
+        safetymaps.infoWindow.initialize(dbkjs.options.separateWindowMode);
+    });
+})();
