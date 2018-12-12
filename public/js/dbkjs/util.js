c61f466552028b2bc2ae82cf8c33d8f7a7f62cb4
Matthijs Laan <matthijsln@gmail.com>
2018-12-12 12:29:34 +0100

    add String.prototype.endsWith polyfill

--- public/js/dbkjs/util.js
+++ public/js/dbkjs/util.js
@@ -64,6 +64,16 @@ if (!String.prototype.repeat) {
   }
 }
 
+/* MDN Polyfill */
+if (!String.prototype.endsWith) {
+	String.prototype.endsWith = function(search, this_len) {
+		if (this_len === undefined || this_len > this.length) {
+			this_len = this.length;
+		}
+		return this.substring(this_len - search.length, this_len) === search;
+	};
+}
+
 var dbkjs = dbkjs || {};
 window.dbkjs = dbkjs;
 
