a6ce78b991f2866ea1fe287a61e36e8a20a7b97e
Matthijs Laan <matthijsln@gmail.com>
2019-01-03 18:16:35 +0100

    implement cluster feature attribute minClusteringResolution, so at lower resolution (deeper zoom) than minClusteringResolution attribute value the feature is not clustered

--- public/js/safetymaps/shared/utils.js
+++ public/js/safetymaps/shared/utils.js
@@ -114,6 +114,7 @@ OpenLayers.Strategy.Cluster.prototype.cluster = function (event) {
         var resolution = this.layer.map.getResolution();
         this.resolution = resolution;
         var clusters = [];
+        var nonClusteredFeatures = [];
         var feature, clustered, cluster;
         for (var i = 0; i < this.features.length; ++i) {
             feature = this.features[i];
@@ -152,19 +153,23 @@ OpenLayers.Strategy.Cluster.prototype.cluster = function (event) {
                         }else{continue;}
                     }else{continue;}
                 }
-                clustered = false;
-                for (var j = clusters.length - 1; j >= 0; --j) {
-                    cluster = clusters[j];
-                    if (this.shouldCluster(cluster, feature)) {
-                        this.addToCluster(cluster, feature);
-                        clustered = true;
-                        break;
+                // Only cluster if resolution above feature.attributes.minClusteringResolution
+                if(feature.attributes.minClusteringResolution && resolution < feature.attributes.minClusteringResolution) {
+                    nonClusteredFeatures.push(feature);
+                } else {
+                    clustered = false;
+                    for (var j = clusters.length - 1; j >= 0; --j) {
+                        cluster = clusters[j];
+                        if (this.shouldCluster(cluster, feature)) {
+                            this.addToCluster(cluster, feature);
+                            clustered = true;
+                            break;
+                        }
+                    }
+                    if (!clustered) {
+                        clusters.push(this.createCluster(this.features[i]));
                     }
                 }
-                if (!clustered) {
-                    clusters.push(this.createCluster(this.features[i]));
-                }
-
             }
         }
         this.clustering = true;
@@ -193,6 +198,11 @@ OpenLayers.Strategy.Cluster.prototype.cluster = function (event) {
         }
         this.clusters = clusters;
 
+        if(nonClusteredFeatures.length > 0) {
+            this.clustering = true;
+            this.layer.addFeatures(nonClusteredFeatures);
+            this.clustering = false;
+        }
     }
 },
 
