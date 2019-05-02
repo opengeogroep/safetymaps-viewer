a2e45c4ae689983b6d6c0e05a5f81e34719a0a08
Martijn <m_struijk1990@hotmail.nl>
2019-05-02 15:30:01 +0200

    Version update

--- public/js/safetymaps/config/options.js
+++ public/js/safetymaps/config/options.js
@@ -7,7 +7,7 @@ OpenLayers.IMAGE_RELOAD_ATTEMPTS = 3;
 dbkjs.options = {
     VERSION: "_VERSION_",
     CONFIG: "_CONFIG_",
-    APPLICATION: "SafetyMaps Viewer 4.3.5-SNAPSHOT (release S3 2018)",
+    APPLICATION: "SafetyMaps Viewer 4.3.5 (release S3 2018)",
 
     zoom: 13,
 
