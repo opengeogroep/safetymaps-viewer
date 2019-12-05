950717cd9faf935164b1593b9037a33d3ab2c424
Matthijs Laan <matthijsln@gmail.com>
2019-12-05 17:11:42 +0100

    bootstrap-slider 4.3.0 -> 10.6.2

--- combine_min_css_js.sh
+++ combine_min_css_js.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cat public/js/libs/bootstrap-3.2.0-dist/css/bootstrap.min.css public/css/typeahead.js-bootstrap.css public/css/slider.css > public/css/libs.min.css
+cat public/js/libs/bootstrap-3.2.0-dist/css/bootstrap.min.css public/css/typeahead.js-bootstrap.css public/css/bootstrap-slider.min.css > public/css/libs.min.css
 cd public/js/libs
 cat jquery-1.11.0/jquery-1.11.0.min.js bootstrap-3.2.0-dist/js/bootstrap.min.js proj4js-compressed.js OpenLayers-2.13.1/OpenLayers.js moment-with-locales.min.js fastclick.min.js bootstrap-slider.js i18next.js jquery.typeahead.js mustache.min.js pdfobject.min.js jsts.min.js > libs.min.js
 
