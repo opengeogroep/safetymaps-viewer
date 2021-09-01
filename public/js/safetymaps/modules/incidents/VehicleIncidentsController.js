b558da531594732868f15710f67edbadea81a9f1
Bart Verhaar <b.verhaar@safetyct.com>
2021-09-01 10:04:06 +0200

    Reset themas because .net does not accept array corrcet

--- public/js/safetymaps/modules/incidents/VehicleIncidentsController.js
+++ public/js/safetymaps/modules/incidents/VehicleIncidentsController.js
@@ -1075,7 +1075,6 @@ VehicleIncidentsController.prototype.logKladblokChatRowToGMS = function (row, in
             incidentId: incidentId,
             content: row,
             themas: me.options.logKladblokToGmsThemas.length > 0 ? me.options.logKladblokToGmsThemas[0] : '',
-            themas: me.options.logKladblokToGmsThemas.length > 1 ? me.options.logKladblokToGmsThemas[1] : '',
             publisher: "safetymaps-viewer",
         },
         xhrFields: { withCredentials: true }, crossDomain: true
